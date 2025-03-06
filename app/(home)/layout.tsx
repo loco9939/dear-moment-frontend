fcadc71a48f488a350c3d86ab649b9028161d1f8
WATA-Yiju <yiju.dev@watanow.com>
2025-03-06 21:54:04 +0900

    feat: header logo 추가

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,8 +1,17 @@
+import { Icon_Logo } from '@/assets/icons';
+import Link from 'next/link';
+
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container space-y-4">
-      <header className="text-2xl font-bold">Dear moment</header>
+    <div className="container">
+      <header className="py-[1.5rem] px-[2rem]">
+        <Link href="/" className="inline-block">
+          <Icon_Logo width={120} height={23} />
+        </Link>
+      </header>
+
       {children}
+
       <section>
         <div>네비게이션</div>
       </section>
